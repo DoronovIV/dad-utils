1531ec0547059ad466821b8830743698dc59ca85
Ivan Snigirev <mensyoo2370@gmail.com>
2024-08-15 22:48:24 +0300

    stats first commit god forgive me (#8)

--- src/utils/pages/calculator/Calculator.tsx
+++ src/utils/pages/calculator/Calculator.tsx
@@ -1,10 +1,42 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './Calculator.scss';
+import { Button } from 'react-bootstrap';
 
 interface CalculatorProps {}
 
 const Calculator: FC<CalculatorProps> = () => {
-  return <div className="calculator"></div>;
+  const [str, setStr] = useState(0);
+
+  return (
+    <div className="calculator">
+      <div className="stats">
+        <div>
+          <div>Strength</div>
+          <div>
+            <Button
+              onClick={() => {
+                setStr(str - 1);
+              }}>
+              {'<'}
+            </Button>
+            {str}
+            <Button
+              onClick={() => {
+                setStr(str + 1);
+              }}>
+              {'>'}
+            </Button>
+          </div>
+        </div>
+        <div>Vigor</div>
+        <div>Agility</div>
+        <div>Dexterity</div>
+        <div>Will</div>
+        <div>Knowledge</div>
+        <div>Resourcefullness</div>
+      </div>
+    </div>
+  );
 };
 
 export default Calculator;
